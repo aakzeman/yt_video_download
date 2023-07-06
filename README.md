5a1902126e3e28e46574d3b757f8c629174ba98a
aakzeman <aakzeman@gmail.com>
2023-07-06 12:07:40 -0400

    Initial project files.

---
+++ README.md
@@ -0,0 +1,3 @@
+### Description
+
+Simple python program utilizing yt-dlp library to download public YouTube playlists to local directory.
---
+++ yt_download.py
@@ -0,0 +1,25 @@
+import yt_dlp
+import os
+
+def download_playlist(url, target_directory):
+    # Ensure the target directory exists
+    if not os.path.exists(target_directory):
+        os.makedirs(target_directory)
+
+    # Define ytdl options
+    ydl_opts = {
+        'format': 'bestvideo+bestaudio/best',
+        'outtmpl': os.path.join(target_directory, '%(title)s.%(ext)s'),
+        'noplaylist': False,
+        'ignoreerrors': True
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+# Define the URL of the playlist and the target directory
+playlist_url = 'https://www.youtube.com/playlist?list=PLRqH2kf18nWgrdMgrFIKn_YuBZmkOnUpR'
+target_directory = 'D:\youtube_download'
+
+# Call the function
+download_playlist(playlist_url, target_directory)
